58a51342b6aa4efe89b087eca26a47d847735c7e
Adam <aw1875@rit.edu>
2018-10-15 20:14:25 -0400

    Pull remote files

--- Homework #7/src/FileCompare.java
+++ Homework #7/src/FileCompare.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.util.*;
 
 public class FileCompare {
+    File file1 = new File("");
+    File file2 = new File("");
+    public static void charByChar (File file1, File file2) throws FileNotFoundException {
+        try {
+            Scanner f1 = new Scanner(file1);
+            Scanner f2 = new Scanner(file2);
+        }
+        catch (FileNotFoundException f) {
+            System.out.print("One or more files not found.");
+        }
+    }
 }

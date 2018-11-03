dad606633777d3d97b9e95713d9375bda771acaa
Adam <aw1875@rit.edu>
2018-11-03 15:20:01 -0400

    Add test puzzle file and completed first constructor

--- Homework #10/src/PuzzleConfig.java
+++ Homework #10/src/PuzzleConfig.java
@@ -1,9 +1,43 @@
-import java.util.Collection;
+import java.io.*;
+import java.util.*;
 
 public class PuzzleConfig implements Configuration {
 
+    /**
+     * Take in a file and construct the initial puzzle from it.
+     * @param filename - file containing puzzle
+     */
     public PuzzleConfig (String filename) {
-
+        int board [][] = new int[9][9];
+        int counter = 0;
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(filename));
+            String input;
+            while ((input = bf.readLine()) != null) {
+                String currLine[] = input.split(" ");
+                for (int i = 0; i < currLine.length; i++) {
+                    if (currLine[i].equals(".")) {
+                        board[counter][i] = 0;
+                    } else {
+                        board[counter][i] = Integer.parseInt(currLine[i]);
+                    }
+                }
+                counter++;
+            }/*
+            String resultBoard = "";
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[i].length; j++) {
+                    resultBoard += board[i][j] + " ";
+                }
+                System.out.println(resultBoard);
+                resultBoard = "";
+            }*/
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Please check the current directory.");
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
     }
 
     public PuzzleConfig (PuzzleConfig config) {
@@ -31,6 +65,8 @@ public class PuzzleConfig implements Configuration {
     }
 
     public static void main (String [] args) {
+        PuzzleConfig p1 = new PuzzleConfig("src/b1.txt");
+
 
     }
 }

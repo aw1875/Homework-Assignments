61ffaf329bfb1b2d5334ff53a4096f340dd46985
Adam <aw1875@rit.edu>
2018-11-08 20:18:42 -0500

    Stubbed out all functions for assignment

---
+++ Homework #11/src/ShortestDistance.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShortestDistance {
+
+    /**
+     * Take in file and construct graph
+     * @param filename - file containing graph info
+     */
+    public ShortestDistance (String filename) {
+        // Construct graph
+    }
+
+
+    /**
+     * Will return graph constructed
+     * @return constructed graph
+     */
+    public Map<String, Node> getGraph() {
+        Map<String, Node> m1 = new HashMap<>();
+        // Return graph
+        return m1;
+    }
+
+    /**
+     * Will print the graph
+     * @return FILL
+     */
+    public String toString() {
+        String result = "";
+        // Return string
+        return result;
+    }
+
+    /**
+     * FILL
+     * @param args - command line arguements
+     */
+    public static void main (String [] args) {
+
+    }
+}
